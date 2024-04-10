296c209480b5d9de177abdd63cbf3aa64d49d5c1
Menzorov_Konstatin <mnz.const@gmail.com>
2024-04-10 18:38:48 +0300

    stage

--- project/task6.py
+++ project/task6.py
@@ -4,8 +4,8 @@ from pyformlang.cfg import CFG
 def cfg_to_wnf(cfg: CFG) -> CFG:
     cfg1 = cfg.eliminate_unit_productions().remove_useless_symbols()
     tmp = cfg1._get_productions_with_only_single_terminals()
-    new_rules = set(cfg1._decompose_productions(tmp))
-    return CFG(start_symbol=cfg1.start_symbol, productions=new_rules)
+    new_prod = cfg1._decompose_productions(tmp)
+    return CFG(start_symbol=cfg1.start_symbol, productions=new_prod)
 
 
 def cfg_from_file(path: str) -> CFG:
